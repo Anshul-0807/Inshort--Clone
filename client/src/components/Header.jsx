9fdb0bf1c4bc09e56428d9233fd81f88a64b85de
Anshul patle <patleanshul920@gmail.com>
2023-04-15 01:01:00 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import {AppBar, Toolbar , styled } from '@mui/material'
-// import {Menu} from '@mui/icons-material';
+import {Menu} from '@mui/icons-material';
 
 const StyledHeader = styled(AppBar)`
 background: #fff;
 height: 70px;
 `;
 
+const MenuIcon = styled(Menu)`
+color: #000;
+`
+
 const Header = () => {
   return (
     <StyledHeader>
         <Toolbar>
-            {/* <Menu/> */}
+            <MenuIcon/>
         </Toolbar>
     </StyledHeader>
   )
