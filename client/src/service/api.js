e189a2fc3c53abc9b9ebbba2f652edc03b24b54b
Anshul patle <patleanshul920@gmail.com>
2023-04-15 23:52:35 +0530

    Create api.js

---
+++ client/src/service/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const getNews = async() => {
+  const URl = "";
+
+  try {
+    return await axios.get(URL);
+  } catch (error) {
+    console.log("Error while calling get news api", error);
+  }
+};
