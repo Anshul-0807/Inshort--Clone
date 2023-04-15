62e4bde0c551e0371c6092a6d0c940aedbc459b6
Anshul patle <patleanshul920@gmail.com>
2023-04-15 23:52:29 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import {AppBar, Toolbar , styled } from '@mui/material'
-import {Menu} from '@mui/icons-material';
+import React from "react";
+import { AppBar, Toolbar, styled } from "@mui/material";
+import { Menu } from "@mui/icons-material";
 
 const StyledHeader = styled(AppBar)`
-background: #fff;
-height: 70px;
+  background: #fff;
+  height: 70px;
 `;
 
 const MenuIcon = styled(Menu)`
-color: #000;
+  color: #000;
 `;
 
-const Image = styled('img')({
+const Image = styled("img")({
   height: 55,
-  margin: 'auto',
+  margin: "auto",
   paddingRight: 70,
-})
+});
 
 const Header = () => {
-
-  const url = 'https://assets.inshorts.com/website_assets/images/logo_inshorts.png';
+  const url =
+    "https://assets.inshorts.com/website_assets/images/logo_inshorts.png";
 
   return (
-    <StyledHeader position='static'>
-        <Toolbar>
-            <MenuIcon/>
-            <Image src={url} alt="icon" />
-        </Toolbar>
+    <StyledHeader position="static">
+      <Toolbar>
+        <MenuIcon />
+        <Image src={url} alt="icon" />
+      </Toolbar>
     </StyledHeader>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
