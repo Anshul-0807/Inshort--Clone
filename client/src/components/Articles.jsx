3ab88a4d74dc091eee0ca66ce9e826ff0ff3b09b
Anshul patle <patleanshul920@gmail.com>
2023-04-15 23:52:26 +0530

    Update Articles.jsx

--- client/src/components/Articles.jsx
+++ client/src/components/Articles.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Box} from '@mui/material'
+import { useEffect } from 'react'
+import { getNews } from '../service/api';
 
 const Articles = () => {
+
+    useEffect(() => {
+      dailyNews();
+    }, [])
+    
+    const dailyNews = async() => {
+       let response = await getNews()
+    }
+
   return (
-    <div>Articles</div>
+    <Box>
+
+    </Box>
   )
 }
 
