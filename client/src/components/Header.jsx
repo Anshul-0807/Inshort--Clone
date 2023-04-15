df0f7b8ce42f75df2913d7b8311624600b7fdddf
Anshul patle <patleanshul920@gmail.com>
2023-04-15 22:57:22 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,13 +9,23 @@ height: 70px;
 
 const MenuIcon = styled(Menu)`
 color: #000;
-`
+`;
+
+const Image = styled('img')({
+  height: 55,
+  margin: 'auto',
+  paddingRight: 70,
+})
 
 const Header = () => {
+
+  const url = 'https://assets.inshorts.com/website_assets/images/logo_inshorts.png';
+
   return (
     <StyledHeader>
         <Toolbar>
             <MenuIcon/>
+            <Image src={url} alt="icon" />
         </Toolbar>
     </StyledHeader>
   )
