5631f9c52c4f5326ca42411dce5abbef116648b1
Anshul patle <patleanshul920@gmail.com>
2023-04-15 23:39:33 +0530

    Update InfoHeader.jsx

--- client/src/components/InfoHeader.jsx
+++ client/src/components/InfoHeader.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, styled } from '@mui/material'
 
+const Container = styled(Box)`
+background: #f44336;
+color: #fff;
+display: flex;
+align-items: center;
+height: 48px;
+`
 
 const InfoHeader = () => {
 
@@ -8,16 +15,30 @@ const InfoHeader = () => {
     const googleStore = 'https://assets.inshorts.com/website_assets/images/playstore.png';
 
 
+const Image = styled('img')({
+  height: 34,
+  '&: last-child': {
+    margin:' 0 50px 0 20px'
+  }
+ 
+})
+ 
+const Text = styled(Typography)`
+ font-size : 14px;
+ font-weight: 300;
+ margin-left: 50px
+`
+
   return (
-    <Box>
-        <Typography>
+    <Container>
+        <Text>
             For the best experience use inshorts in your smartphones
-        </Typography>
-        <Box>
-            <img src={appleStore} alt="" />
-            <img src={googleStore} alt="" />
+        </Text>
+        <Box style={{display: 'flex', marginLeft: 'auto'}}>
+            <Image src={appleStore} alt="appleStore" />
+            <Image src={googleStore} alt="googleStore" />
         </Box>
-    </Box>
+    </Container>
   )
 }
 
