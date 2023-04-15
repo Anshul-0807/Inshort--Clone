b67578d6c4956cdaf831f2e85872e459b6650f57
Anshul patle <patleanshul920@gmail.com>
2023-04-15 23:09:04 +0530

    Update InfoHeader.jsx

--- client/src/components/InfoHeader.jsx
+++ client/src/components/InfoHeader.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Box, Typography } from '@mui/material'
+
 
 const InfoHeader = () => {
+
+    const appleStore = 'https://assets.inshorts.com/website_assets/images/appstore.png';
+    const googleStore = 'https://assets.inshorts.com/website_assets/images/playstore.png';
+
+
   return (
-    <div>InfoHeader</div>
+    <Box>
+        <Typography>
+            For the best experience use inshorts in your smartphones
+        </Typography>
+        <Box>
+            <img src={appleStore} alt="" />
+            <img src={googleStore} alt="" />
+        </Box>
+    </Box>
   )
 }
 
