9bcc12b1b7d1997b9cd03b46c67e9aeb1ed15ecc
Anshul patle <patleanshul920@gmail.com>
2023-04-15 23:08:53 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
-
+import {Box} from '@mui/material'
 
 // component
 import Header from './components/Header';
+import InfoHeader from './components/InfoHeader';
+
 
 function App() {
   return (
-    <div >
+    <Box >
       <Header/>
-    </div>
+      <InfoHeader/>
+    </Box>
   );
 }
 
