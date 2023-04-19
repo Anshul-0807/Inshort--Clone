9fcb430c78a9a020c0aad3c6ce71bc248d3733d5
Anshul patle <patleanshul920@gmail.com>
2023-04-20 00:44:32 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
-import './App.css';
-import {Box, styled} from '@mui/material'
+import "./App.css";
+import { Box, styled } from "@mui/material";
 
 // component
-import Header from './components/Header';
-import InfoHeader from './components/InfoHeader';
-import Articles from './components/Articles';
+import Header from "./components/Header";
+import InfoHeader from "./components/InfoHeader";
+import Articles from "./components/Articles";
 
- const Container = styled(Box)`
- width: 60%;
- margin: 40px auto 0 auto;
- `
- 
+const Container = styled(Box)`
+  width: 60%;
+  margin: 40px auto 0 auto;
+`;
+
 function App() {
   return (
-    <Box >
-      <Header/>
-     <Container>
-      <InfoHeader/>
-      <Articles/>
-    </Container>
-
-    </Box>  
+    <Box>
+      <Header />
+      <Container>
+        <InfoHeader />
+        <Articles />
+      </Container>
+    </Box>
   );
 }
 
