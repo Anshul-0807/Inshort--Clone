c29f826345f1147ab7897ec95b00da548356b6ce
Anshul patle <patleanshul920@gmail.com>
2023-04-20 22:33:09 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,8 +1,14 @@
+import mongoose from "mongoose";
 
+const Connection = async () => {
+  const URL = `mongodb://anshulblog:charlieap@ac-0yy82dc-shard-00-00.z10hvu3.mongodb.net:27017,ac-0yy82dc-shard-00-01.z10hvu3.mongodb.net:27017,ac-0yy82dc-shard-00-02.z10hvu3.mongodb.net:27017/?ssl=true&replicaSet=atlas-qngscj-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
+  try {
+    await mongoose.connect(URL, { useNewUrlParser: true });
+    console.log("Database connected successfully")
+  } catch (error) {
+    console.log("Error while connecting the database", error);
+  }
+};
 
-
-const Connection = () => {
-    
-}
-
+export default Connection;
