d347494af97498c3d689eb9265a9cb2bcc302c30
Anshul patle <patleanshul920@gmail.com>
2023-04-24 22:46:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Connection from "./database/db.js";
-
+import DefaultData from "./default.js";
 
 
 const app = express()
@@ -11,3 +11,5 @@ const PORT = 8000
 Connection()
 
 app.listen(PORT, () => console.log(`server running successfully on port ${PORT}`))
+
+DefaultData()
