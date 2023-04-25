aa0be3e2c3fa09163ec9b246724ad18774e3df8d
Anshul patle <patleanshul920@gmail.com>
2023-04-25 23:33:30 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -4,7 +4,7 @@ export const getNews = async () => {
   const URl = "http://localhost:8000";
 
   try {
-    return await axios.get(URL);
+    return await axios.get(`${URl}/news`);
   } catch (error) {
     console.log("Error while calling get news api", error);
   }
