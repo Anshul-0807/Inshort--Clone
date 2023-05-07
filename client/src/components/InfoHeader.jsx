96a08f8afdd314f0fbfa5657f3bda89ab8778d69
Anshul patle <patleanshul920@gmail.com>
2023-05-07 22:03:36 +0530

    Update InfoHeader.jsx

--- client/src/components/InfoHeader.jsx
+++ client/src/components/InfoHeader.jsx
@@ -1,45 +1,42 @@
-import React from 'react'
-import { Box, Typography, styled } from '@mui/material'
+import React from "react";
+import { Box, Typography, styled } from "@mui/material";
 
 const Container = styled(Box)`
-background: #f44336;
-color: #fff;
-display: flex;
-align-items: center;
-height: 48px;
-`
+  background: #f44336;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  height: 48px;
+`;
 
 const InfoHeader = () => {
+  const appleStore =
+    "https://assets.inshorts.com/website_assets/images/appstore.png";
+  const googleStore =
+    "https://assets.inshorts.com/website_assets/images/playstore.png";
 
-    const appleStore = 'https://assets.inshorts.com/website_assets/images/appstore.png';
-    const googleStore = 'https://assets.inshorts.com/website_assets/images/playstore.png';
+  const Image = styled("img")({
+    height: 34,
+    "&: last-child": {
+      margin: " 0 50px 0 20px",
+    },
+  });
 
+  const Text = styled(Typography)`
+    font-size: 14px;
+    font-weight: 300;
+    margin-left: 50px;
+  `;
 
-    const Image = styled('img')({
-        height: 34,
-        '&: last-child': {
-            margin: ' 0 50px 0 20px'
-        }
+  return (
+    <Container>
+      <Text>For the best experience use inshorts in your smartphones</Text>
+      <Box style={{ display: "flex", marginLeft: "auto" }}>
+        <Image src={appleStore} alt="appleStore" />
+        <Image src={googleStore} alt="googleStore" />
+      </Box>
+    </Container>
+  );
+};
 
-    })
-
-    const Text = styled(Typography)`
- font-size : 14px;
- font-weight: 300;
- margin-left: 50px
-`
-
-    return (
-        <Container>
-            <Text>
-                For the best experience use inshorts in your smartphones
-            </Text>
-            <Box style={{ display: 'flex', marginLeft: 'auto' }}>
-                <Image src={appleStore} alt="appleStore" />
-                <Image src={googleStore} alt="googleStore" />
-            </Box>
-        </Container>
-    )
-}
-
-export default InfoHeader
+export default InfoHeader;
