061c45e8b6b57cb15c98d78b5d20ed98c86ddd24
Anshul patle <patleanshul920@gmail.com>
2023-05-07 22:03:56 +0530

    Update default.js

--- server/default.js
+++ server/default.js
@@ -2,6 +2,7 @@ import { data } from "./constants/data.js";
 import News from "./model/new-schema.js";
 const DefaultData = async () => {
   try {
+    
    await News.insertMany(data);
    console.log("Data import successfully")
   } catch (error) {
