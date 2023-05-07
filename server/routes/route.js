08284a08c7f9cb0aa8316ad00cf7d2f6a2c587bf
Anshul patle <patleanshul920@gmail.com>
2023-05-07 22:04:06 +0530

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -2,8 +2,8 @@ import express from "express";
 
 const route = express.Router();
 
-route.get('/news', () => {
-    console.log("hello")
-})
+route.get("/news", () => {
+  console.log("hello");
+});
 
 export default route;
